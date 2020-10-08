3203c36bc5d90eb8adc3747685b936d317738dec
wenhua <yao.wen@husky.neu.edu>
2020-10-07 17:02:47 -0700

    added client part1 part2

---
+++ newAssignment1/a1ClientPart2/src/main/java/Statistics.java
@@ -0,0 +1,41 @@
+public class Statistics {
+  private double mean;
+  private double median;
+  private double p99;
+  private double max;
+
+  public Statistics() {
+  }
+
+  public double getMean() {
+    return mean;
+  }
+
+  public void setMean(double mean) {
+    this.mean = mean;
+  }
+
+  public double getMedian() {
+    return median;
+  }
+
+  public void setMedian(double median) {
+    this.median = median;
+  }
+
+  public double getP99() {
+    return p99;
+  }
+
+  public void setP99(double p99) {
+    this.p99 = p99;
+  }
+
+  public double getMax() {
+    return max;
+  }
+
+  public void setMax(double max) {
+    this.max = max;
+  }
+}
